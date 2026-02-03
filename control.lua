9ecbb3a850b50af865673503f7295745cfdf83dd
Subject-314159 <168562337+Subject-314159@users.noreply.github.com>
2026-02-03 22:15:25 +0100

    wip

--- control.lua
+++ control.lua
@@ -16,7 +16,7 @@ script.on_init(function()
     -- We consider games that are >10sec old to be an existing save
     if game.tick > 10 * 60 then
         game.print(
-            "[color=red][font=bold]WARNING[/font][/color] It appears that Science Is Fake was added to an existing save, be aware that your production chain might be broken!")
+            "[color=red]WARNING[/color] It appears that Science Is Fake was added to an existing save, be aware that your production chain might be broken!")
     end
 end)
 
